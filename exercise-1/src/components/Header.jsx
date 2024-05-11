194c0f2647a9eebb913cf12316797a7af8ad7cf0
hue ho <hthi53503@gmail.com>
2024-05-11 16:24:02 +0700

    complete C2-S3-PRACTICE

---
+++ exercise-1/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export function Header(){
+    return (
+        <>
+          <header className="block">
+            <h2>Welcome to this course!</h2>
+          </header>
+        </>
+      );
+}
---
+++ exercise2/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export function Footer(){
+    return (
+        <>
+          <footer className="block">
+            <h2>Welcome to this course!</h2>
+          </footer>
+        </>
+      );
+}
